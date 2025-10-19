95968818e050a352aa4404370fc2e2eba4c16d2f
Ethan E. <eastwoode375@gmail.com>
2025-10-19 14:26:05 -0500

    Create Minimal API Proj, Add Model / Context

---
+++ TaskTrackerAPI/Data/Models/Todo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskTrackerAPI.Data.Models;
+
+public class Todo
+{
+    public int Id { get; set; }
+
+    [Required, StringLength(200)]
+    public string Title { get; set; } = string.Empty;
+
+    [StringLength(1000)]
+    public string? Description { get; set; }
+
+    public DateTime? DueDate { get; set; }
+
+    public bool IsComplete { get; set; }
+}
---
+++ TaskTrackerAPI/Data/TodoDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskTrackerAPI.Data.Models;
+
+namespace TaskTrackerAPI.Data;
+
+public class TodoDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Todo> TodoList { get; set; }
+}
