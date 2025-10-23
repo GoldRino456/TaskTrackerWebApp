3f0d5e9a2b756704aec9893da0b5d13d1e12252b
Ethan E. <eastwoode375@gmail.com>
2025-10-23 14:50:01 -0500

    Minimal API First Implementation

--- TaskTrackerAPI/Program.cs
+++ TaskTrackerAPI/Program.cs
@@ -1,16 +1,78 @@
 using Microsoft.EntityFrameworkCore;
 using TaskTrackerAPI.Data;
+using TaskTrackerAPI.Data.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//Services Go Here
 builder.Services.AddDbContext<TodoDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")
     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
 builder.Services.AddEndpointsApiExplorer();
 
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+}
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var todoItems = app.MapGroup("/todoitems");
+todoItems.MapGet("/", GetAllTodos);
+todoItems.MapGet("/complete", GetCompleteTodos);
+todoItems.MapGet("/{id}", GetTodo);
+todoItems.MapPost("/", CreateTodo);
+todoItems.MapPut("/{id}", UpdateTodo);
+todoItems.MapDelete("/{id}", DeleteTodo);
 
 app.Run();
+
+static async Task<IResult> GetAllTodos(TodoDbContext db)
+{
+    return TypedResults.Ok(await db.TodoList.ToArrayAsync());
+}
+
+static async Task<IResult> GetCompleteTodos(TodoDbContext db)
+{
+    return TypedResults.Ok(await db.TodoList.Where(t => t.IsComplete).ToListAsync());
+}
+
+static async Task<IResult> GetTodo(int id, TodoDbContext db)
+{
+    return await db.TodoList.FindAsync(id) is Todo todo ? TypedResults.Ok(todo) : TypedResults.NotFound();
+}
+
+static async Task<IResult> CreateTodo(Todo todo, TodoDbContext db)
+{
+    db.TodoList.Add(todo);
+    await db.SaveChangesAsync();
+    return TypedResults.Created($"/todoitems/{todo.Id}", todo);
+}
+
+static async Task<IResult> UpdateTodo(int id, Todo inputTodo, TodoDbContext db)
+{
+    var todo = await db.TodoList.FindAsync(id);
+
+    if (todo is null)
+        return TypedResults.NotFound();
+
+    todo.Title = inputTodo.Title;
+    todo.Description = inputTodo.Description;
+    todo.DueDate = inputTodo.DueDate;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+}
+
+static async Task<IResult> DeleteTodo(int id, TodoDbContext db)
+{
+    if (await db.TodoList.FindAsync(id) is Todo todo)
+    {
+        db.TodoList.Remove(todo);
+        await db.SaveChangesAsync();
+        return TypedResults.NoContent();
+    }
+
+    return TypedResults.NotFound();
+}
